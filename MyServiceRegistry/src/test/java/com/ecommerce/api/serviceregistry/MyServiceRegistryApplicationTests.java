b515ac6b0d159d59803209f30969bc6beef90659
aditya <aditya@DESKTOP-DMI79QV>
2021-07-12 22:51:28 +0530

    Committing Service Registry

---
+++ MyServiceRegistry/src/test/java/com/ecommerce/api/serviceregistry/MyServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.api.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
